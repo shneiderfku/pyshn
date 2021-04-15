bf0bfb4b7e03beb22c08ceda75cb6f2cae3b15aa
Rafael Ribeiro <shneiderfku@outlook.com>
2021-04-15 19:42:57 -0300

    Added CNPJ command line interface. Closes #6

--- src/pyshn/cli.py
+++ src/pyshn/cli.py
@@ -1,4 +1,5 @@
 from pyshn.cpf import cpf as _cpf, CpfError
+from pyshn.cnpj import cnpj as _cnpj, CnpjError
 import pyshn
 import click
 
@@ -49,4 +50,48 @@ def cpf(number:str, generate:bool, no_validate:bool, format_spec:str):
 
         click.echo(output)
     except CpfError as e:
+        click.echo(message=f"pyshn: {e}")
+
+@main.command()
+@click.argument('number', 
+    nargs=1, 
+    type=str
+)
+@click.option('--format', 'format_spec',
+    type=click.Choice(['s', 'N', 'n']),
+    default='s',
+    help="""s: the default format.                                  
+    N: the new format (displayed in the latest documents).         
+    n: the numeric format (show only digits)."""
+)
+@click.option('--generate',
+    is_flag=True,
+    default=False,
+    help="Generate the checksum."
+)
+@click.option('--no-validate', 
+    is_flag=True, 
+    default=False,
+    help="Don't validate the CNPJ number."
+)
+def cnpj(number:str, generate:bool, no_validate:bool, format_spec:str):
+    """
+    Format, validate or generate CNPJ's numbers.
+    """
+    try:
+        if number.isnumeric():
+            number=int(number)
+        
+        if format_spec == 's':
+            format_spec = ''
+        
+        result = _cnpj(number, generate=generate, validate=False)
+        output = format(result, format_spec)
+
+        if not generate and no_validate == False:
+            output += " "
+            output += "(valid)" if result else "(invalid)"
+        
+        click.echo(output)
+    except CnpjError as e:
         click.echo(message=f"pyshn: {e}")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,6 +8,12 @@ def test_pyshn_cpf():
     assert result.exit_code == 0
     assert result.output == '000.000.001-91 (valid)\n'
 
+def test_pyshn_cnpj():
+    runner = CliRunner()
+    result = runner.invoke(main, ['cnpj', '191'])
+    assert result.exit_code == 0
+    assert result.output == '00.000.000/0001-91 (valid)\n'
+
 def test_pyshn_cpf_format():
     runner = CliRunner()
     with runner.isolation():
@@ -23,6 +29,17 @@ def test_pyshn_cpf_format():
         assert result.exit_code == 0
         assert result.output == '000000001/91 (valid)\n'
 
+def test_pyshn_cnpj_format():
+    runner = CliRunner()
+    with runner.isolation():
+        result = runner.invoke(main, ['cnpj', '--format', 's', '191'])
+        assert result.exit_code == 0
+        assert result.output == '00.000.000/0001-91 (valid)\n'
+    with runner.isolation():
+        result = runner.invoke(main, ['cnpj', '--format', 'n', '191'])
+        assert result.exit_code == 0
+        assert result.output == '00000000000191 (valid)\n'
+
 def test_pyshn_cpf_no_validate():
     runner = CliRunner()
     with runner.isolation():
@@ -34,14 +51,37 @@ def test_pyshn_cpf_no_validate():
         assert result.exit_code == 0
         assert result.output == '000.000.000-01\n'
 
+def test_pyshn_cnpj_no_validate():
+    runner = CliRunner()
+    with runner.isolation():
+        result = runner.invoke(main, ['cnpj', '--no-validate', '191'])
+        assert result.exit_code == 0
+        assert result.output == '00.000.000/0001-91\n'
+    with runner.isolation():
+        result = runner.invoke(main, ['cnpj', '--no-validate', '1'])
+        assert result.exit_code == 0
+        assert result.output == '00.000.000/0000-01\n'
+
 def test_pyshn_cpf_generate():
     runner = CliRunner()
     result = runner.invoke(main, ['cpf', '--generate', '1'])
     assert result.exit_code == 0
     assert result.output == '000.000.001-91\n'
 
+def test_pyshn_cnpj_generate():
+    runner = CliRunner()
+    result = runner.invoke(main, ['cnpj', '--generate', '1'])
+    assert result.exit_code == 0
+    assert result.output == '00.000.000/0001-91\n'
+
 def test_pyshn_cpf_invalid_format():
     runner = CliRunner()
     result = runner.invoke(main, ['cpf', 'x'])
     assert result.exit_code == 0
     assert result.output == "pyshn: invalid format 'x'.\n"
+
+def test_pyshn_cnpj_invalid_format():
+    runner = CliRunner()
+    result = runner.invoke(main, ['cnpj', 'x'])
+    assert result.exit_code == 0
+    assert result.output == "pyshn: cnpj: invalid format 'x'\n"
